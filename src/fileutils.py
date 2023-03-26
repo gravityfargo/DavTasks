75dedc1dfbdf929d59294242f69d0827114d2b85
gravityfargo <gravityfargo@pm.me>
2023-03-26 19:53:06 -0400

    Save json to home directory

--- src/fileutils.py
+++ src/fileutils.py
@@ -2,7 +2,6 @@ import json
 import os
 from datetime import date, datetime
 
-# dataFile = "./src/localData.json"
 dataFile = os.path.expanduser('~/.DavTasksData.json')
 settingsAr = {"URL": "", "USERNAME": "",
               "PASSWORD": "", "CALENDARS": "", "ENABLEDCALENDARS": "", "DEFAULTCAL": "", "LASTSYNC": ""}
